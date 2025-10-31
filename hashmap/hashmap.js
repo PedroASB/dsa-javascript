08ccde7047a6edce1ed3361178ab0f3b32cc85e6
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-10-31 15:53:35 -0300

    feat(hashmap): implement hashmap module
    - Note: growth feature not implemented

---
+++ hashmap/hashmap.js
@@ -0,0 +1,197 @@
+class Node {
+  constructor(key = null, value = null, nextNode = null) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class HashMap {
+  /* This HashMap only handle keys of type string */
+  #buckets;
+  #length;
+  #capacity;
+  #loadFactor;
+  #threshold;
+
+  constructor(capacity = 16, loadFactor = 0.75) {
+    this.#capacity = capacity;
+    this.#loadFactor = loadFactor;
+    this.#threshold = capacity * loadFactor;
+    this.#buckets = new Array(capacity);
+    this.#length = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 23;
+
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.#capacity;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.#buckets.length) {
+      throw Error('Attempt to access index out of bounds');
+    }
+
+    let currentNode = this.#buckets[index];
+
+    while (currentNode) {
+      // if the node already exists, update it
+      if (currentNode.key === key) {
+        currentNode.value = value;
+        return;
+      }
+      currentNode = currentNode.nextNode;
+    }
+
+    // prepending the node at buckets[index] list
+    const newNode = new Node(key, value, this.#buckets[index] || null);
+    this.#buckets[index] = newNode;
+    this.#length++;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.#buckets.length) {
+      throw Error('Attempt to access index out of bounds');
+    }
+
+    let currentNode = this.#buckets[index];
+
+    while (currentNode) {
+      if (currentNode.key === key) {
+        return currentNode.value;
+      }
+      currentNode = currentNode.nextNode;
+    }
+
+    return null;
+  }
+
+  has(key) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.#buckets.length) {
+      throw Error('Attempt to access index out of bounds');
+    }
+
+    let currentNode = this.#buckets[index];
+
+    while (currentNode) {
+      if (currentNode.key === key) {
+        return true;
+      }
+      currentNode = currentNode.nextNode;
+    }
+
+    return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.#buckets.length) {
+      throw Error('Attempt to access index out of bounds');
+    }
+
+    let previousNode = null;
+    let currentNode = this.#buckets[index];
+
+    while (currentNode) {
+      if (currentNode.key === key) {
+        if (previousNode) {
+          previousNode.nextNode = currentNode.nextNode;
+        } else if (currentNode.nextNode) {
+          if (currentNode.nextNode) this.#buckets[index] = currentNode.nextNode;
+        } else {
+          delete this.#buckets[index];
+        }
+        this.#length--;
+        return true;
+      }
+      previousNode = currentNode;
+      currentNode = currentNode.nextNode;
+    }
+
+    return false;
+  }
+
+  length() {
+    return this.#length;
+  }
+
+  clear() {
+    this.#buckets = new Array(this.#capacity);
+    this.#length = 0;
+  }
+
+  keys() {
+    const keys = [];
+
+    for (const index in this.#buckets) {
+      let currentNode = this.#buckets[index];
+      while (currentNode) {
+        keys.push(currentNode.key);
+        currentNode = currentNode.nextNode;
+      }
+    }
+
+    return keys;
+  }
+
+  values() {
+    const values = [];
+
+    for (const index in this.#buckets) {
+      let currentNode = this.#buckets[index];
+      while (currentNode) {
+        values.push(currentNode.value);
+        currentNode = currentNode.nextNode;
+      }
+    }
+
+    return values;
+  }
+
+  entries() {
+    const entries = [];
+
+    for (const index in this.#buckets) {
+      let currentNode = this.#buckets[index];
+      while (currentNode) {
+        entries.push([currentNode.key, currentNode.value]);
+        currentNode = currentNode.nextNode;
+      }
+    }
+
+    return entries;
+  }
+
+  toString() {
+    let string = '';
+
+    for (const index in this.#buckets) {
+      string += `${index}: `;
+      let currentNode = this.#buckets[index];
+
+      while (currentNode) {
+        string += `( '${currentNode.key}', ${currentNode.value} ) -> `;
+        currentNode = currentNode.nextNode;
+      }
+
+      string += 'null\n';
+    }
+
+    return string;
+  }
+}
+
+export default HashMap;
